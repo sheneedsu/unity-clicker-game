0c6b803e422a0ca81760a29b9d69a94457fca40d
DESKTOP-CA4E1DC\SHE <shneedsu@gmail.com>
2017-11-25 18:17:05 +0900

    완성

---
+++ Assets/Scripts/GoldCounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldCounter : MonoBehaviour {
+
+    public int Gold = 0;
+    public Text textGold;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(StartGoldCount());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator StartGoldCount() {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime (0.1f) ;
+            Gold++;
+            textGold.text = Gold.ToString();
+        }
+    }
+
+
+}
